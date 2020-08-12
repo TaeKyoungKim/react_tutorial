0f94c3bf9d0d6f69e1437e6a0b831f884b958da5
GaryKim <moduedu@gmail.com>
2020-08-12 14:39:51 +0900

    props.children 활용

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Hello from './Hello'
+import Wrapper from './Wrapper'
 
 import './App.css';
 
 function App() {
   return (
-      <>
+    
+      <Wrapper>
       <Hello name="김태경"  color="red" />
       <Hello  color="blue" />
-      </>
+      </Wrapper>
   );
 }
 
