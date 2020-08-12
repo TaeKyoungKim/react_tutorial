1d40d80b18aae9c93160404717238e2caa775e26
GaryKim <moduedu@gmail.com>
2020-08-12 16:35:20 +0900

    useRef 활용 예제

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,6 +1,6 @@
 //다중 input 상태 관리 useState 와 onChange
 
-import React , {useState} from 'react'
+import React , {useState , useRef} from 'react'
 
 function InputSample(){
     const [text, setText] = useState({
@@ -8,6 +8,10 @@ function InputSample(){
         nickname:''
     })
     
+     // DOM구조에 특정 위치에 어떤 처리를 하고 싶을떄
+    //  Reset버튼 클릭시 name input에 focus가 가있도록 하기 위해 useRef활용
+    const nameInput = useRef();
+    // 다중 input 관리
     const {name , nickname} = text
     const onChange=(e)=>{
         // console.log(e.target)
@@ -22,11 +26,13 @@ function InputSample(){
         setText({
             name:'',
             nickname:''
-        })
+        });
+
+        nameInput.current.focus();
     }
     return (
         <div>
-            <input name="name" value={name} onChange={onChange} placeholder={'입력하세요'}/>
+            <input name="name" value={name} onChange={onChange} placeholder={'입력하세요'} ref={nameInput}/>
             <input name="nickname" value={nickname} onChange={onChange} placeholder={'닉네임을 입력하세요'}/>
             <button onClick={onReset} >초기화</button>
             <div>
