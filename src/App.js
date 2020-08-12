6888345d634bd083cafff2ffc7cec1fce1dbad30
GaryKim <moduedu@gmail.com>
2020-08-12 15:33:35 +0900

    useState 활용한 state update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import Hello from './Hello'
 import Wrapper from './Wrapper'
+import Counter from './Counter'
 
 import './App.css';
 
 
 function App() {
   return (
-    
+      <>
       <Wrapper>
       <Hello name="김태경"  color="red" isSpecial={true} />
       <Hello  color="blue" />
       </Wrapper>
+      <Counter />
+      </>
   );
 }
 
