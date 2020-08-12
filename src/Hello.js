654652eaf78e341a1da58889c06d6df79edcddc4
GaryKim <moduedu@gmail.com>
2020-08-12 14:25:21 +0900

    defaultProps 활용

--- src/Hello.js
+++ src/Hello.js
@@ -1,9 +1,18 @@
 // props를 활용하는 기본 방법
+// defaultProps 활용한 방법
 
 import React from 'react';
 
-function Hello(props){
-    return <div style={{color:props.color}}> 안녕하세요 {props.name}  </div>
+// function Hello(props){
+//     return <div style={{color:props.color}}> 안녕하세요 {props.name}  </div>
+// }
+
+function Hello({color , name}){
+    return <div style={{color}}> 안녕하세요 {name}  </div>
+}
+
+Hello.defaultProps = {
+    name:"대전"
 }
 
 export default Hello;
